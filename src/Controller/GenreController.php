84ca8579f240e85ebc8f227160ebc71d656decad
unknown <camillemichaudmeli@gmail.com>
2022-10-04 16:40:28 +0200

    04102022_Genre_Composer_Post/Put

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -6,12 +6,17 @@ use Serializable;
 use App\Entity\Genre;
 use App\Repository\GenreRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 
 class GenreController extends AbstractController
 {
@@ -47,7 +52,7 @@ class GenreController extends AbstractController
      * @param SerializerInterface $serializer
      * @return JsonResponse
      */
-    #[Route('/api/genre/{idGenre}', name:'genre.getOne', methods: ['GET'])]
+    #[Route('/api/genre/{idGenre}', name:'genre.get', methods: ['GET'])]
     #[ParamConverter("genre", options : ["id"=> "idGenre"])]
     public function getGenreById(Genre $genre, SerializerInterface $serializer): JsonResponse
     {
@@ -88,5 +93,54 @@ class GenreController extends AbstractController
         $entityManager->flush();
 
         return new JsonResponse(null, Response::HTTP_OK, [], false);
+    }
+
+    #[Route('/api/genre', name: 'genre.create', methods: ['POST'])]
+    public function createGenre(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator, ValidatorInterface $validator): JsonResponse
+    {
+        $genre = $serializer->deserialize(
+            $request->getContent(),
+            Genre::class,
+            "json"
+        );
+        $genre->setStatus(true);
+
+        // $content = $request->toArray();
+        // rajouter le repo de l'entité concerné
+        // $trucatrouver = $repoEnquestion->find($content['idDutruc'] ?? -1);
+        // $genre->setAuthor($trucatrouver);
+
+        $error = $validator->validate($genre);
+        if($error->count() > 0) {
+            return new JsonResponse($serializer->serialize($error, 'json'), Response::HTTP_BAD_REQUEST, [], true);
+        }
+
+        $entityManager->persist($genre);
+        $entityManager->flush();
+
+        $jsonGenre = $serializer->serialize($genre, "json", ['groups' => 'getGenre']);
+        $location = $urlGenerator->generate('genre.get', ['idGenre' => $genre->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonGenre, Response::HTTP_CREATED, ["location" => $location], true);
+    }
+
+    #[Route('/api/genre/{idGenre}', name: 'genre.update', methods: ['PUT'])]
+    #[ParamConverter("genre", options: ["id" => "idGenre"])]
+    public function updateGenre(Genre $genre, Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator): JsonResponse
+    {
+        $genreUpdate = $serializer->deserialize(
+            $request->getContent(),
+            Genre::class,
+            "json",
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $genre]
+        );
+        $genreUpdate->setStatus(true);
+        $entityManager->persist($genreUpdate);
+        $entityManager->flush();
+
+        $jsonGenre = $serializer->serialize($genreUpdate, 'json', ['groups' => 'getGenre']);
+        $location = $urlGenerator->generate('genre.get', ['idGenre' => $genreUpdate->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonGenre, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 }
